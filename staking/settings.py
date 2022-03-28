d7977e95bcb59f12ff2004f827a176ad2a3d927a
datlabeswatini <99124741+markbookpro69@users.noreply.github.com>
2022-03-28 21:20:07 +0200

    settings update

--- staking/settings.py
+++ staking/settings.py
@@ -188,7 +188,9 @@ CELERY_RESULT_BACKEND = 'django-db'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 
-#if os.getcwd() == '/app':
- #   SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
- #   SECURE_SSL_REDIRECT = True
- #   DEBUG=False
+if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    PREPEND_WWW = True
+    BASE_URL = "https://www.crypto-coinstaking.com"
+ #  DEBUG=False
