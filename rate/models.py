75b63c4a89ccce130c7629f1c63fa7ecc9fcab91
datlabeswatini <99124741+markbookpro69@users.noreply.github.com>
2022-03-29 17:09:20 +0200

    UPDATE RATE

--- rate/models.py
+++ rate/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Rates models here.
 
 class Rate(models.Model):
-    interest_rate = models.DecimalField(max_digits = 15, decimal_places = 3,)
+    interest_rate = models.DecimalField(max_digits = 15, decimal_places = 4,)
     affiliate_rate = models.IntegerField(null=True, blank=True)
     minimum_withdrawal = models.DecimalField(max_digits = 15, decimal_places = 2,)
     minimum_investment = models.DecimalField(max_digits = 15, decimal_places = 2,)
