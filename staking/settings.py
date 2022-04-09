fea576923e12c72268b5aa5f140245fd1f314e54
datlabeswatini <99124741+markbookpro69@users.noreply.github.com>
2022-04-09 04:51:30 +0200

    email update 2

--- staking/settings.py
+++ staking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import socket
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -164,7 +165,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 #SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = socket.gethostbyname('smtp.gmail.com')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
