ec6b529c7253789f1729b8714c59e128901b08b0
datlabeswatini <99124741+markbookpro69@users.noreply.github.com>
2022-04-09 07:18:47 +0200

    v3

--- staking/settings.py
+++ staking/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['crypto-coinstaking.com', 'crypto-coinstaking.herokuapp.com', 'www.crypto-coinstaking.com', '127.0.0.1']
 
