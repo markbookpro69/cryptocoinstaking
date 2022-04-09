27f59347d5cac17a0c83647bbe7eb8714e79a7fd
datlabeswatini <99124741+markbookpro69@users.noreply.github.com>
2022-04-09 04:47:53 +0200

    email update

--- staking/settings.py
+++ staking/settings.py
@@ -167,9 +167,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_USE_SSL = Truee
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ['EMAIL_EMAIL']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
+DEFAULT_FROM_EMAIL = "info@crypto-coinstaking.com"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
